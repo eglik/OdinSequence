18dbe88c83038302927e119f3f00c0b66b9bd506
wolfi <wolfi@bytenine.io>
2020-03-23 12:27:47 +0900

    feat: Sequence Creator

---
+++ Assets/Quat/Scripts/Editor/SequenceCreator.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+
+public class SequenceCreator : MonoBehaviour
+{
+    [MenuItem("Sequence/Create New Sequence")]
+    public static void CreateTemp()
+    {
+        string content = File.ReadAllText($"{Application.dataPath}/d/Scripts/Temp/TempSequence.cs");
+        content = content.Replace("TempSequence", "NewSequence");
+        File.WriteAllText($"{Application.dataPath}/NewSequence.cs", content, Encoding.UTF8);
+        AssetDatabase.Refresh();
+    }
+}
